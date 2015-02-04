d0ad50b877c7c7c1d5d4692b976b3affb4a94c89
dquilter <david_quilter@hotmail.com>
2015-02-04 15:41:14 +0000

    Updated basic Node boilerplate.

--- boilerplate-node.js
+++ boilerplate-node.js
@@ -5,13 +5,16 @@ var http = require('http');
 var host = '127.0.0.1';
 var port = 8000;
 
+// Create your app using the createServer method.
 var app = http.createServer(function(request, response) {
-	response.writeHead(200, {
-	  'Content-Type': 'text/plain' });
+	// The headers for your responses will need to be written.
+	// Notice status code 200 and the Content-Type being specified.
+	response.writeHead(200, { 'Content-Type': 'text/plain' });
+	// Sends data to be output (as per the headers).
 	response.write('This is a basic Node application');
+	// Indicates that we have finished sending our response.
 	response.end();
-})
-
+});
 
 // The application can be opened on http://127.0.0.1:8000. 
 // Notice the host and host and port variables which were set earlier.
